db2252947ee70681609e52cdf42a2cf00db41d78
JHDev2006 <howelljoe2@gmail.com>
2026-03-18 20:01:27 +0000

    fixed object detectors not working great on blocks

--- Scripts/Classes/Gizmos/PlayerDetection.gd
+++ Scripts/Classes/Gizmos/PlayerDetection.gd
@@ -25,7 +25,7 @@ func _draw() -> void:
 	draw_rect(Rect2(Vector2(-size_x / 2.0, -size_y / 2.0) * 16, Vector2(size_x, size_y) * 16), Color.WHITE, false, 1.0)
 
 func run_check() -> void:
-	if get_tree().paused or is_inside_tree() == false:
+	if get_tree().paused or is_inside_tree() == false or Global.level_editor_is_editing():
 		return
 	var save = object_in_area
 	object_in_area = false
