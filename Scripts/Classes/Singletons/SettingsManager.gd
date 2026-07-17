1536f1670a98e14b93e079762225e0d692d4d3fc
JHDev2006 <howelljoe2@gmail.com>
2026-07-17 14:04:11 +0100

    fixed angry sun not having a proper death gib

--- Scripts/Classes/Singletons/SettingsManager.gd
+++ Scripts/Classes/Singletons/SettingsManager.gd
@@ -182,7 +182,7 @@ func on_window_resized() -> void:
 	var window_size = get_viewport().get_window().size
 	if get_viewport().get_window().mode == Window.Mode.MODE_MAXIMIZED:
 		Settings.file.video.mode = 1
-	elif get_viewport().get_window().mode == Window.Mode.MODE_WINDOWED:
+	elif get_viewport().get_window().mode == Window.Mode.MODE_WINDOWED and Settings.file.video.mode < 2:
 		Settings.file.video.mode = 0
 	Settings.file.video.window_size[0] = window_size.x
 	Settings.file.video.window_size[1] = window_size.y
