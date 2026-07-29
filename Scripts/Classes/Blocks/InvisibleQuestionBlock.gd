af72ec31eba31a841efb9c44d7fa885968ad49bb
JHDev2006 <howelljoe2@gmail.com>
2026-07-29 16:31:32 +0100

    fixed being able to hit invis question blocks from the top

--- Scripts/Classes/Blocks/InvisibleQuestionBlock.gd
+++ Scripts/Classes/Blocks/InvisibleQuestionBlock.gd
@@ -9,7 +9,7 @@ func on_area_entered(area: Area2D) -> void:
 func upside_down_area_entered(area: Area2D) -> void:
 	if area.owner is Player:
 		var player: Player = area.owner
-		if player.velocity.y > 0 and player.global_position.y < $Hitbox.global_position.y and abs(player.global_position.x - global_position.x) < 8:
+		if player.velocity.y > 0 and player.global_position.y < $Hitbox.global_position.y and abs(player.global_position.x - global_position.x) < 8 and player.gravity_vector == Vector2.UP:
 			player_hit(player)
 
 func player_hit(player: Player) -> void:
